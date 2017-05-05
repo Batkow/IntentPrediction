2994d2149a7ccab729e12a1821799cb5ff3522a4
Batkow <ivobatkovic@gmail.com>
2017-05-05 10:50:37 +0200

    the script to store the values

--- Python/gridworld.py
+++ Python/gridworld.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import copy
 import sys
 
-img = plt.imread('intersection.png')
+img = plt.imread('10x10grid.png')
 
 def dynamics(x,u,dt):
 	xNext = np.array([x[0] + dt * np.cos(u),
@@ -21,8 +21,8 @@ def valueIteration():
 
 	global img
 
-	gridHeight = 100
-	gridWidth = 100
+	gridHeight = 10
+	gridWidth = 10
 	nActions = 24
 	discount = 0.99
 	nIterations = 100
@@ -40,47 +40,112 @@ def valueIteration():
 
 	value = R.copy()
 
-	for k in range(nIterations):
-		print(k)
-		print(np.min(value))
+	while True:
+
+
+		valueNew = value.copy()
 		for xi in range(gridWidth):
 			for yi in range(gridHeight):
 				if ((xi == (gridWidth - 1)) and (yi == (gridWidth - 1))):
 					continue
 				Xnew = dynamics([xi, yi],actions,1)
-				vals = cv2.remap(value, Xnew[0,:].astype('float32'), Xnew[1,:].astype('float32'), cv2.INTER_NEAREST)
-				vals[vals == 0] = -1000 # temp fix for invalid locations
+				vals = cv2.remap(value, Xnew[0,:].astype('float32'), Xnew[1,:].astype('float32'), cv2.INTER_NEAREST, borderValue = -1000)
+				#vals[vals == 0] = -1000 # temp fix for invalid locations
 				total = R[yi,xi] + discount * np.max(vals)
-				value[yi,xi] = total
+				valueNew[yi,xi] = total
 
-	# valueImage = cv2.normalize(value, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-	# cv2.imshow('value', valueImage)
-	# cv2.waitKey(0)
+		normError = np.linalg.norm(value-valueNew)
+		print normError
+
+		if (normError < 0.0001):
+			break
+
+		value = valueNew.copy()
+
+
+
+	valueImage = cv2.normalize(value, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+	cv2.imshow('value', valueImage)
+	cv2.waitKey(0)
 	np.save('value.npy',value)
+	return value
+
+
+def runValueIterations(name,goalX,goalY):
+
+	global img
+
+	gridHeight = 10
+	gridWidth = 10
+	nActions = 24
+	discount = 0.99
+	nIterations = 100
+     
+	actions = np.linspace(0,2*math.pi,nActions)
+
+	rChannel = (img[:,:,0] == 1)
+	gChannel = (img[:,:,1] == 1)
+	bChannel = (img[:,:,2] == 1)
+
+	R = np.zeros(img[:,:,0].shape)
+	R[rChannel] = -2
+	R[gChannel] = -0.5
+	R[goalY,goalX] = 0.0
+
+	value = R.copy()
+
+	while True:
+
+
+		valueNew = value.copy()
+		for xi in range(gridWidth):
+			for yi in range(gridHeight):
+				if ((xi == (goalX)) and (yi == (goalY))):
+					continue
+				Xnew = dynamics([xi, yi],actions,1)
+				vals = cv2.remap(value, Xnew[0,:].astype('float32'), Xnew[1,:].astype('float32'), cv2.INTER_NEAREST, borderValue = -1000)
+				#vals[vals == 0] = -1000 # temp fix for invalid locations
+				total = R[yi,xi] + discount * np.max(vals)
+				valueNew[yi,xi] = total
+
+		normError = np.linalg.norm(value-valueNew)
+		print normError
+
+		if (normError < 0.0001):
+			break
+
+		value = valueNew.copy()
+
+
+
+	valueImage = cv2.normalize(value, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+	cv2.imshow('value', valueImage)
+	cv2.waitKey(1)
+	np.save('valueFolder/value' + name + '.npy',value)
 	return value
 
 
 def runAgent():
 
 	global img
-	value = np.load('value.npy')
+	value = np.load('valueFolder/value9x9.npy')
 	#value = valueIteration(img)
-	implot = plt.imshow(img)
+	implot = plt.imshow(value)
 
 	nActions = 24
 	nSteps = 2000
 	actions = np.linspace(0,2*math.pi,nActions)
-	x = np.array([[1.1], [100.0]])
+	x = np.array([[1.1], [1.1]])
 	v = 1
-	dt = 1
+	dt = 1.0
 	X = x
 	u = []
 	for k in range(nSteps):
 	  xNew = dynamics(x,actions,dt)
-	  vals = cv2.remap(value, xNew[0,:].astype('float32'), xNew[1,:].astype('float32'), cv2.INTER_NEAREST)
-	  vals[vals == 0] = -1000
+	  vals = cv2.remap(value, xNew[0,:].astype('float32'), xNew[1,:].astype('float32'), cv2.INTER_NEAREST,borderValue = -1000)
+	  #vals[vals == 0] = -1000
 	  maxVal = np.max(vals)
-	  probs = np.exp(200*(vals-maxVal))
+	  probs = np.exp(1000.0*(vals-maxVal))
 	  totalProb = np.sum(probs)
 	  cumProb = np.cumsum(probs/totalProb)
 	  
@@ -92,7 +157,7 @@ def runAgent():
 	  x = xNew[:,idx]
 	  
 	  X = np.append(X,[[x[0]],[x[1]]],axis=1)
-	  if (np.linalg.norm(x-np.array([[100],[100]])) < 1):
+	  if (np.linalg.norm(x-np.array([[9],[9]])) < 1):
 	  	break
 	  u.append(actions[idx])
 
@@ -101,8 +166,14 @@ def runAgent():
 
 
 
-valueIteration()
+#valueIteration()
 runAgent()
+
+# for x in range(0,10):
+# 	for y in range(0,10):
+# 		name = str(x) + 'x' + str(y)
+# 		runValueIterations(name,x,y)
+
 
 # still not perfectly explored the map, but works. tweak later. add the negative reward for every time step.
 
